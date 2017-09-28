fbc65d38ceabd5f930d58df16d7dea4eac59126b
kimseunghun <ksh407288@naver.com>
2017-09-28 16:59:12 +0900

    2017 09 28

---
+++ iot/src/report/kimseunghun/r0013/KakaoTest.java
@@ -0,0 +1,15 @@
+package report.kimseunghun.r0013;
+
+import java.util.Arrays;
+
+public class KakaoTest {
+	public int[] solution(int[][] v) {
+        int[] answer = {1,10};
+        System.out.println(v);
+        return answer;
+    }
+	public static void main(String[] args){
+		KakaoTest kt = new KakaoTest();
+		kt.solution(int[1]);
+	}
+}
